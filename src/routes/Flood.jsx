92237c88a6b074f5be0534ffe0492ef7f8bb5e6e
Ankan Das <127923458+ankan20@users.noreply.github.com>
2023-08-19 15:21:43 +0530

    Update Flood.jsx

--- src/routes/Flood.jsx
+++ src/routes/Flood.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import "./flood.css";
 function Flood(){
     return
     <>    
         <Navbar/>
-    <h1>FLOOD ALERT</h1>
+        <div id="flood-main">
+            <h1>FLOOD ALERT</h1>
+        </div>
         <Footer/>
     </>
 }
