11903baa56733f55003328341b2ae24f0b8d9de9
Ankan Das <127923458+ankan20@users.noreply.github.com>
2023-08-22 00:44:47 +0530

    Update Flood.jsx

--- src/routes/Flood.jsx
+++ src/routes/Flood.jsx
@@ -1,3 +1,124 @@
+// import React, { useState, useEffect } from 'react';
+// import "./Poacher.css";
+// import TableData from '../Components/TableData';
+// import Navbar from "../Components/Navbar";
+// import Footer from "../Components/Footer";
+// import "./flood.css";
+
+// function Flood(){
+//   const [waterLevel, setWaterLevel] = useState('');
+//   const [responseMessage, setResponseMessage] = useState('');
+//   const [isLoading, setLoading] = useState(false);
+//   const [floodData,setFloodData]=useState([]);
+
+//   const handlelevelChange = (e) => {
+//     setWaterLevel(e.target.value);
+//   };
+
+//   const callFloodApi = async () => {
+//     try {
+//         if (!waterLevel) {
+//             window.alert('Please enter the water level');
+//             return;
+//           }
+//         setLoading(true);
+//       const response = await fetch('https://firewaterapi.onrender.com/flooddetector', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({  floodsensordata:parseInt(waterLevel) }), // Send temperature as JSON
+//       });
+//       setLoading(false);
+//       if (!response.ok) {
+//         throw new Error('Failed to fetch API');
+//       }
+
+//       const data = await response.json();
+//       setResponseMessage(data); // Display API response message
+//     } catch (error) {
+//       console.error('Error:', error);
+//       setResponseMessage('An error occurred.');
+//     }
+//   };
+
+//   const getFloodData=async()=>{
+//     try{
+//         const res=await fetch("https://auth-handler-forestwatchai.onrender.com/floodData",{
+//             method:"GET",
+//             headers:{
+//                 "Content-Type":"application/json",
+//                 'Accept': 'application/json'
+//             }
+//         });
+
+//         if (!res.ok) {
+//             throw new Error("Request failed");
+//         }
+        
+//         const data=await res.json();
+//         if(data.length > 0){
+//             setFloodData(data);
+//         }
+//         console.log(data);
+//     }catch(err){
+//         console.log(`The error is:${err}`);
+//     }
+//   }
+//   useEffect(()=>{
+//     getFloodData();
+//   },[responseMessage]);
+
+//   return (
+//     <>
+//       <Navbar/>
+//       <div id="flood-1">
+//             <div id="flood-main">
+//       <h1>FLood Alert</h1>
+//       <h2>Input data to test the feature:</h2>
+//         <input type="number" value={waterLevel} placeholder='Enter water level' onChange={handlelevelChange} />
+//       <button onClick={callFloodApi}>Submit</button>
+//       {isLoading && (
+//         <div className="loading-spinner"></div>
+//       )}
+//       {responseMessage && !isLoading && (
+//         <div>
+//           <h2>API Response:</h2>
+//           <p>{responseMessage}</p>
+//         </div>
+//       )}
+//       <div>
+//       <div className="dataTable_heading">
+//             <h1>Past Water Level Data:</h1>
+//       </div>
+//         <table>
+//             <thead>
+//                 <tr>
+//                     <th>Water Level</th>
+//                     <th>Capture Time</th>
+//                     <th>Capture Location</th>
+//                 </tr>
+//             </thead>
+//             <tbody>
+//                 <TableData value={floodData}/>
+//             </tbody>
+//         </table>
+//       </div>
+//       </div>
+//         </div>
+//         <Footer/>
+//     </>
+//   );
+// }
+
+// export default Flood;
+
+
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import "./Poacher.css";
 import TableData from '../Components/TableData';
@@ -5,108 +126,107 @@ import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import "./flood.css";
 
-function Flood(){
+function Flood() {
   const [waterLevel, setWaterLevel] = useState('');
   const [responseMessage, setResponseMessage] = useState('');
   const [isLoading, setLoading] = useState(false);
-  const [floodData,setFloodData]=useState([]);
+  const [floodData, setFloodData] = useState([]);
 
-  const handlelevelChange = (e) => {
+  const handleLevelChange = (e) => {
     setWaterLevel(e.target.value);
   };
 
   const callFloodApi = async () => {
     try {
-        if (!waterLevel) {
-            window.alert('Please enter the water level');
-            return;
-          }
-        setLoading(true);
+      if (!waterLevel) {
+        window.alert('Please enter the water level');
+        return;
+      }
+      setLoading(true);
       const response = await fetch('https://firewaterapi.onrender.com/flooddetector', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({  floodsensordata:parseInt(waterLevel) }), // Send temperature as JSON
+        body: JSON.stringify({ floodsensordata: parseInt(waterLevel) }), // Send water level as JSON
       });
       setLoading(false);
+
       if (!response.ok) {
         throw new Error('Failed to fetch API');
       }
 
       const data = await response.json();
-      setResponseMessage(data); // Display API response message
+      setResponseMessage(data.message); // Display API response message
     } catch (error) {
       console.error('Error:', error);
       setResponseMessage('An error occurred.');
     }
   };
 
-  const getFloodData=async()=>{
-    try{
-        const res=await fetch("https://auth-handler-forestwatchai.onrender.com/floodData",{
-            method:"GET",
-            headers:{
-                "Content-Type":"application/json",
-                'Accept': 'application/json'
-            }
-        });
+  const getFloodData = async () => {
+    try {
+      const res = await fetch("https://auth-handler-forestwatchai.onrender.com/floodData", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          'Accept': 'application/json'
+        }
+      });
 
-        if (!res.ok) {
-            throw new Error("Request failed");
-        }
-        
-        const data=await res.json();
-        if(data.length > 0){
-            setFloodData(data);
-        }
-        console.log(data);
-    }catch(err){
-        console.log(`The error is:${err}`);
+      if (!res.ok) {
+        throw new Error("Request failed");
+      }
+
+      const data = await res.json();
+      setFloodData(data);
+    } catch (err) {
+      console.log(`The error is:${err}`);
     }
   }
-  useEffect(()=>{
+
+  useEffect(() => {
     getFloodData();
-  },[responseMessage]);
+  }, []);
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div id="flood-1">
-            <div id="flood-main">
-      <h1>FLood Alert</h1>
-      <h2>Input data to test the feature:</h2>
-        <input type="number" value={waterLevel} placeholder='Enter water level' onChange={handlelevelChange} />
-      <button onClick={callFloodApi}>Submit</button>
-      {isLoading && (
-        <div className="loading-spinner"></div>
-      )}
-      {responseMessage && !isLoading && (
-        <div>
-          <h2>API Response:</h2>
-          <p>{responseMessage}</p>
-        </div>
-      )}
-      <div>
-      <div className="dataTable_heading">
-            <h1>Past Water Level Data:</h1>
-      </div>
-        <table>
-            <thead>
+        <div id="flood-main">
+          <h1>Flood Alert</h1>
+          <h2>Input data to test the feature:</h2>
+          <input type="number" value={waterLevel} placeholder='Enter water level' onChange={handleLevelChange} />
+          <button onClick={callFloodApi}>Submit</button>
+          {isLoading && (
+            <div className="loading-spinner"></div>
+          )}
+          {responseMessage && !isLoading && (
+            <div>
+              <h2>API Response:</h2>
+              <p>{responseMessage}</p>
+            </div>
+          )}
+          <div>
+            <div className="dataTable_heading">
+              <h1>Past Water Level Data:</h1>
+            </div>
+            <table>
+              <thead>
                 <tr>
-                    <th>Water Level</th>
-                    <th>Capture Time</th>
-                    <th>Capture Location</th>
+                  <th>Water Level</th>
+                  <th>Capture Time</th>
+                  <th>Capture Location</th>
                 </tr>
-            </thead>
-            <tbody>
-                <TableData value={floodData}/>
-            </tbody>
-        </table>
-      </div>
-      </div>
+              </thead>
+              <tbody>
+                <TableData value={floodData} />
+              </tbody>
+            </table>
+          </div>
         </div>
-        <Footer/>
+      </div>
+      <Footer />
     </>
   );
 }
